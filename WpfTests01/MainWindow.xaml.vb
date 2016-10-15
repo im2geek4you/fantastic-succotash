3ebce503eb3b78712401791860f4cc76316089e3
im2geek4you <im2geek4you@gmail.com>
2016-10-15 19:35:52 +0100

    Test project

---
+++ WpfTests01/MainWindow.xaml.vb
@@ -0,0 +1,12 @@
+﻿Class MainWindow 
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Me.Close()
+    End Sub
+
+
+    Private Sub Ellipse_MouseDown(sender As Object, e As MouseButtonEventArgs)
+        Me.DragMove()
+    End Sub
+
+End Class
